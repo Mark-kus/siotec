a66523110cf196708e277d1346f524f14ae9df4b
Marco <tignanellimarco@gmail.com>
2023-07-05 19:26:43 -0300

    custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: '#467D9B',
+				header: '#181E25',
+				footer: '#181E25'
+			},
+		},
 	},
 	plugins: [],
 }
