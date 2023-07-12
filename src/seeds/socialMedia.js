ae1e4bbac2da6679780948a8782a456fc2175725
Marco <tignanellimarco@gmail.com>
2023-07-11 23:16:38 -0300

    socialMedia seeds

---
+++ src/seeds/socialMedia.js
@@ -0,0 +1,24 @@
+import facebook from "/public/socialMedia/facebook.svg";
+import instagram from "/public/socialMedia/instagram.svg";
+import whatsapp from "/public/socialMedia/whatsapp.svg";
+
+export default [
+    {
+        id: 1,
+        name: "Facebook",
+        url: "https://www.facebook.com/",
+        image: facebook
+    },
+    {
+        id: 2,
+        name: "Instagram",
+        url: "https://www.instagram.com/",
+        image: instagram
+    },
+    {
+        id: 3,
+        name: "Whatsapp",
+        url: "https://api.whatsapp.com/send/?phone=%2B5492326412131",
+        image: whatsapp
+    },
+]
