660b16014a2c2ad500c7ff4ced1eb47161ca5708
Marco <tignanellimarco@gmail.com>
2023-07-13 23:14:03 -0300

    change variable name

--- src/seeds/socialMedia.js
+++ src/seeds/socialMedia.js
@@ -3,18 +3,18 @@ export default [
         id: 1,
         name: "Facebook",
         url: "https://www.facebook.com/",
-        image: "fa-brands fa-facebook fa-2xl"
+        icon: "fa-brands fa-facebook fa-2xl"
     },
     {
         id: 2,
         name: "Instagram",
         url: "https://www.instagram.com/",
-        image:  "fa-brands fa-instagram fa-2xl"
+        icon:  "fa-brands fa-instagram fa-2xl"
     },
     {
         id: 3,
         name: "Whatsapp",
         url: "https://api.whatsapp.com/send/?phone=%2B5492326412131",
-        image: "fa-brands fa-whatsapp fa-2xl"
+        icon: "fa-brands fa-whatsapp fa-2xl"
     },
 ]
