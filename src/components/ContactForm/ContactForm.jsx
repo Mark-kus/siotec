5464ab115b2eedf26f54d452540a8a74858d75eb
Marco <tignanellimarco@gmail.com>
2023-07-14 00:21:41 -0300

    replace common tags to semantic tags

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -16,50 +16,74 @@ export default function ContactForm() {
     return (
         <form onSubmit={handleSubmit(data => onSubmit(data))} className="flex flex-col w-full m-auto md:w-1/2">
 
-            <label className={labelCls}>
-                Nombre - Empresa
-                <input {...register("name", {
-                    required: "Este campo es requerido",
-                    maxLength: {
-                        value: 40,
-                        message: "El nombre debe tener menos de 40 caracteres"
-                    },
-                })} type="text"
-                    className={`${inputCls} ${errors.name?.message ? "" : "md:rounded-b"} mt-0`} />
-            </label>
-            <p className={`${errorCls} ${errors.name?.message ? "opacity-100 p-1" : ""}`}>{errors.name?.message}</p>
+            <div className="form-group">
+                <label htmlFor="name" className={labelCls}>
+                    Nombre - Empresa
+                </label>
+                <input
+                    {...register("name", {
+                        required: "Este campo es requerido",
+                        maxLength: {
+                            value: 40,
+                            message: "El nombre debe tener menos de 40 caracteres"
+                        },
+                    })}
+                    type="text"
+                    id="name"
+                    className={`${inputCls} ${errors.name?.message ? "" : "md:rounded-b"} mt-0`}
+                />
+                <p className={`${errorCls} ${errors.name?.message ? "opacity-100 p-1" : ""}`}>{errors.name?.message}</p>
+            </div>
 
-            <label className={labelCls}>
-                Teléfono
-                <input {...register("phone", {
-                    required: "Este campo es requerido",
-                })} type="number"
-                    className={`${inputCls} ${errors.phone?.message ? "" : "md:rounded-b"}`} />
-            </label>
-            <p className={`${errorCls} ${errors.phone?.message ? "opacity-100 p-1" : ""}`}>{errors.phone?.message}</p>
+            <div className="form-group">
+                <label htmlFor="phone" className={labelCls}>
+                    Teléfono
+                </label>
+                <input
+                    {...register("phone", {
+                        required: "Este campo es requerido",
+                    })}
+                    type="number"
+                    id="phone"
+                    className={`${inputCls} ${errors.phone?.message ? "" : "md:rounded-b"}`}
+                />
+                <p className={`${errorCls} ${errors.phone?.message ? "opacity-100 p-1" : ""}`}>{errors.phone?.message}</p>
+            </div>
 
-            <label className={labelCls}>
-                Email
-                <input {...register("email", {
-                    required: "Este campo es requerido"
-                })} type="email"
-                    className={`${inputCls} ${errors.email?.message ? "" : "md:rounded-b"}`} />
-            </label>
-            <p className={`${errorCls} ${errors.email?.message ? "opacity-100 p-1" : ""}`}>{errors.email?.message}</p>
+            <div className="form-group">
+                <label htmlFor="email" className={labelCls}>
+                    Email
+                </label>
+                <input
+                    {...register("email", {
+                        required: "Este campo es requerido"
+                    })}
+                    type="email"
+                    id="email"
+                    className={`${inputCls} ${errors.email?.message ? "" : "md:rounded-b"}`}
+                />
+                <p className={`${errorCls} ${errors.email?.message ? "opacity-100 p-1" : ""}`}>{errors.email?.message}</p>
+            </div>
 
-            <label className={labelCls}>
-                Consulta
-                <textarea {...register("message", {
-                    required: "Este campo es requerido",
-                })} type="text"
-                    className={`${inputCls} ${errors.message?.message ? "" : "md:rounded-b"} resize-none`} />
-            </label>
-            <p className={`${errorCls} ${errors.message?.message ? "opacity-100 p-1" : ""}`}>{errors.message?.message}</p>
+            <div className="form-group">
+                <label htmlFor="message" className={labelCls}>
+                    Consulta
+                </label>
+                <textarea
+                    {...register("message", {
+                        required: "Este campo es requerido",
+                    })}
+                    id="message"
+                    className={`${inputCls} ${errors.message?.message ? "" : "md:rounded-b"} resize-none`}
+                />
+                <p className={`${errorCls} ${errors.message?.message ? "opacity-100 p-1" : ""}`}>{errors.message?.message}</p>
+            </div>
 
-            <button className="dark:bg-black bg-blue-700 text-white mt-2 p-2">
+            <button className="dark:bg-black bg-blue-700 text-white mt-2 p-2" type="submit">
                 Enviar
             </button>
 
         </form>
+
     )
 }
