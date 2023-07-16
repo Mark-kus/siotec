6bd803509197ed030bc98a68c6ea5a665c2164e1
Marco <tignanellimarco@gmail.com>
2023-07-16 19:13:56 -0300

    update of service's seeds

--- src/seeds/servicios.ts
+++ src/seeds/servicios.ts
@@ -1,33 +1,63 @@
 interface Trabajo {
   id: number;
   title: string;
-  description: string;
+  items: string[];
   images: string[];
 }
 
 const trabajos: Trabajo[] = [
   {
     id: 1,
-    title: "Refrigeracion",
-    description: "Trabajos de refrigeracion",
+    title: "ELECTROMECÁNICA",
+    items: ["Proyecto y Diseño de Instalaciones Eléctricas", "Informes Electromecánicos", "Certificaciones Eléctricas"],
     images: []
   },
   {
     id: 2,
-    title: "Mantenimiento",
-    description: "Trabajos de Mantenimiento",
+    title: "DESARROLLO WEB",
+    items: ["Desarrollo del Frontend", "Desarrollo del Backend", "Desarrollo de APIs", "Mantenimiento y Actualizaciones"],
     images: []
   },
   {
     id: 3,
-    title: "Construccion",
-    description: "Trabajos de Construccion",
+    title: "MANTENIMIENTO INDUSTRIAL",
+    items: ["Servicio de Gerenciamiento de Lubricación", "Diseño y Montaje de Sistemas de Lubricación Centralizados Automáticos", "Servicio de Alineación de Ejes", "Montaje y Desmontaje de Rodamientos", "Análisis de vibración", "Termografía"],
     images: []
   },
   {
     id: 4,
-    title: "Perreo intenso",
-    description: "Trabajos de refrigeracion",
+    title: "ELECTRICIDAD DOMICILIARIA Y COMERCIAL",
+    items: ["Instalación de Cámaras de Seguridad", "Ejecución, Reparación y Ampliación de Instalaciones Eléctricas Monofásicas y Trifásicas.", "Auxilios Eléctricos"],
+    images: []
+  },
+  {
+    id: 5,
+    title: "ELECTRONICA DE CONTROL",
+    items: ["Instalación, Mantenimiento y Reparación de Semáforos", "Instalación, Mantenimiento y Reparación de Balizamiento LED y Sistemas Lumínicos Sonoros para Fuerzas de Seguridad, Cuerpos de Bomberos, Servicios de Salud, Grúas, Seguridad Vial", "Reparación de Electrificadores"],
+    images: []
+  },
+  {
+    id: 6,
+    title: "ELECTRICIDAD INDUSTRIAL",
+    items: ["Ejecución, Reparación, Ampliación y Mantenimiento de Sistemas Eléctricos Trifásicos", "Diseño y Armado de Tableros Eléctricos Trifásicos", "Configuración de Equipos de Automatismo", "Iluminación de Plantas Industriales"],
+    images: []
+  },
+  {
+    id: 7,
+    title: "REFRIGERACIÓN COMERCIAL E INDUSTRIAL",
+    items: ["Armado, Montaje, Instalación y Puesta en Funcionamiento de Cámaras Frigoríficas", "Reparación de Cámaras Frigoríficas", "Reparación de Heladeras Comerciales", "Mantenimiento de Sistemas Frigoríficos Industriales", "Aplicación de Válvulas y Automatismos en Sistemas Frigoríficos Industriales", "Instalación, Mantenimiento y Reparación de Compresores Frigoríficos", "Instalación, Mantenimiento y Reparación de Enfriadores de Aire, Torres de Enfriamiento de Agua y Condensadores", "Montaje de Cañerías y Recipientes en Instalaciones Frigoríficas Industriales"],
+    images: []
+  },
+  {
+    id: 8,
+    title: "GENERADORES ELÉCTRICAS",
+    items: ["Asesoramiento para posible ubicación, Montaje, Instalación y Puesta en Servicio", "Tendido de Cableado de Fuerza y Señales", "Montaje, Instalación y Puesta en Servicio de Equipos", "Instalación de Accesorios para la correcta Conmutación de Cargas", "Conexionado de las Líneas del Equipo a las Líneas del Establecimiento", "Ensayos y Simulacros necesarios para garantizar el correcto funcionamiento", "Mantenimiento Preventivo y Programado de Equipos", "Reparación de Equipos"],
+    images: []
+  },
+  {
+    id: 9,
+    title: "TECNOLOGÍA DE LOS ALIMENTOS",
+    items: ["Organización y gestión de las actividades del laboratorio de producción, de los distintos procesos de producción y/o del desarrollo de nuevos productos, conformes a las normas de higiene, seguridad y ambiente en el procesamiento de los alimentos", "Realización e interpretación , análisis y ensayos organolépticos, físicos, químicos, fisicoquímicos y microbiológicos de materias primas, insumos, materiales en proceso y productos alimenticios (de origen animal, vegetal, mineral y/o artificial), efluentes y emisiones al medio ambiente", "Aplicación, control y ejecución de normas de higiene y seguridad ambientales, inocuidad, inspección e integridad a fin de alcanzar los estándares definidos en la producción y comercialización de los distintos tipos de alimentos", "Aplicación en líneas de producción: BPM, HACCP, POES, Sellado y etiquetado de productos"],
     images: []
   },
 ];
