54e0a542e8cbb594b1549706c9599320084c6886
Marco <tignanellimarco@gmail.com>
2023-07-16 16:55:32 -0300

    change default font to Roboto

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,13 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily : {
+				"sans": ['Roboto', 'sans-serif'],
+			},
 			colors: {
 				light: {
 					page: {
-						background: '#FFFFFF',
+						background: '#FAFAFA',
 						text: "#333333",
 						border: "#333333",
 					},
@@ -23,25 +26,25 @@ module.exports = {
 					error: "#FF1E1E",
 					input: "#0F3460",
 				},
-				dark: {
-					page: {
-						background: '#071952',
-						text: "#FFFFFF",
-						border: "#46458C",
-					},
-					header: {
-						background: '#001C30',
-						text: "#FFFFFF",
-						border: "#46458C",
-					},
-					footer: {
-						background: '#1D5B79',
-						text: "#E5E5E5",
-						border: "#46458C",
-					},
-					error: "#ED2B2A",
-					input: "#F7E6C4",
-				}
+				// dark: {
+				// 	page: {
+				// 		background: '#071952',
+				// 		text: "#FFFFFF",
+				// 		border: "#46458C",
+				// 	},
+				// 	header: {
+				// 		background: '#001C30',
+				// 		text: "#FFFFFF",
+				// 		border: "#46458C",
+				// 	},
+				// 	footer: {
+				// 		background: '#1D5B79',
+				// 		text: "#E5E5E5",
+				// 		border: "#46458C",
+				// 	},
+				// 	error: "#ED2B2A",
+				// 	input: "#F7E6C4",
+				// }
 			}
 		},
 	},
