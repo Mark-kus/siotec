d144fa9a077e63b1238b5174cf8145dad7ad4047
Marco <tignanellimarco@gmail.com>
2023-07-21 21:15:13 -0300

    add new information to web data

--- src/seeds/socialMedia.ts
+++ src/seeds/socialMedia.ts
@@ -9,19 +9,19 @@ const socialMedia: SocialMedia[] = [
     {
         id: 1,
         name: "Facebook",
-        url: "https://www.facebook.com/",
+        url: "https://www.facebook.com/profile.php?id=100094701349474",
         icon: "fa-brands fa-facebook fa-2xl"
     },
     {
         id: 2,
         name: "Instagram",
-        url: "https://www.instagram.com/",
+        url: "https://www.instagram.com/gruposiotec/",
         icon:  "fa-brands fa-instagram fa-2xl"
     },
     {
         id: 3,
         name: "Whatsapp",
-        url: "https://api.whatsapp.com/send/?phone=%2B5492326412131",
+        url: "https://api.whatsapp.com/send/?phone=%2B5491132057207",
         icon: "fa-brands fa-whatsapp fa-2xl"
     },
 ]
