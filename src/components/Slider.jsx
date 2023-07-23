a5c62521a0e22e5e53a104ac277a1ddeb1b7e7ac
Marco <tignanellimarco@gmail.com>
2023-07-23 15:48:21 -0300

    create and render slider with placeholder data

---
+++ src/components/Slider.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const Slider = ({ data }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handlePrevSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : data.length - 1));
+    };
+
+    const handleNextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex < data.length - 1 ? prevIndex + 1 : 0));
+    };
+
+    const slidePercentage = 100 / data.length;
+
+    return (
+        <div className="relative overflow-hidden">
+            <div
+                className="flex transition-all duration-500"
+                style={{
+                    transform: `translateX(-${currentIndex * slidePercentage}%)`,
+                    width: `${data.length * 100}%`,
+                }}
+            >
+                {data.map((item, index) => (
+                    <div key={index} className="w-1/3 p-4">
+                        <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+                            <img src={item.image} alt={item.name} className="w-full h-64 object-cover" />
+                            <div className="p-4">
+                                <h3 className="text-xl font-semibold mb-2">{item.name}</h3>
+                                <p className="text-gray-600 mb-4">{item.description}</p>
+                                <p className="text-blue-600 font-semibold">{item.contact}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <button
+                onClick={handlePrevSlide}
+                className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-500 text-white rounded-full p-2"
+            >
+                &lt;
+            </button>
+            <button
+                onClick={handleNextSlide}
+                className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-500 text-white rounded-full p-2"
+            >
+                &gt;
+            </button>
+        </div>
+    );
+};
+
+export default Slider;
---
+++ src/seeds/integrantes.ts
@@ -0,0 +1,47 @@
+interface Integrantes {
+    image: string
+    name: string
+    contact: string
+    description: string
+}
+
+const data: Integrantes[] = [
+    {
+        image: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        name: 'Nombre 1',
+        contact: 'Contacto 1',
+        description: 'Descripción 1',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        name: 'Nombre 2',
+        contact: 'Contacto 2',
+        description: 'Descripción 2',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        name: 'Nombre 3',
+        contact: 'Contacto 3',
+        description: 'Descripción 3',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        name: 'Nombre 4',
+        contact: 'Contacto 4',
+        description: 'Descripción 4',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        name: 'Nombre 5',
+        contact: 'Contacto 5',
+        description: 'Descripción 5',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        name: 'Nombre 6',
+        contact: 'Contacto 6',
+        description: 'Descripción 6',
+    },
+];
+
+export default data
