a19b4b5491596c8c207f398c54148bf05dc1860e
Marco <tignanellimarco@gmail.com>
2023-08-13 14:07:57 -0300

    clean service page for modified service page

--- src/seeds/servicios.ts
+++ src/seeds/servicios.ts
@@ -1,60 +1,36 @@
-import type { Trabajo } from "../interfaces/trabajo.interface";
+import type { Servicio } from "../interfaces/servicio.interface";
 
-const trabajos: Trabajo[] = [
+const servicios: Servicio[] = [
   {
-    id: 1,
     title: "ELECTROMECÁNICA",
-    items: ["Proyecto y Diseño de Instalaciones Eléctricas", "Informes Electromecánicos", "Certificaciones Eléctricas"],
-    images: []
+    description: "Ofrecemos servicios de reparacion de automoviles y muchas cosas, como lorem ipsum e ipsum lorem",
+    icon: "fa-regular fa-wrench"
   },
   {
-    id: 2,
-    title: "DESARROLLO WEB",
-    items: ["Desarrollo del Frontend", "Desarrollo del Backend", "Desarrollo de APIs", "Mantenimiento y Actualizaciones"],
-    images: []
+    title: "ELECTROMECÁNICA",
+    description: "Ofrecemos servicios de reparacion de automoviles y muchas cosas, como lorem ipsum e ipsum lorem",
+    icon: "fa-regular fa-wrench"
   },
   {
-    id: 3,
-    title: "MANTENIMIENTO INDUSTRIAL",
-    items: ["Servicio de Gerenciamiento de Lubricación", "Diseño y Montaje de Sistemas de Lubricación Centralizados Automáticos", "Servicio de Alineación de Ejes", "Montaje y Desmontaje de Rodamientos", "Análisis de vibración", "Termografía"],
-    images: []
+    title: "ELECTROMECÁNICA",
+    description: "Ofrecemos servicios de reparacion de automoviles y muchas cosas, como lorem ipsum e ipsum lorem",
+    icon: "fa-regular fa-wrench"
   },
   {
-    id: 4,
-    title: "ELECTRICIDAD DOMICILIARIA Y COMERCIAL",
-    items: ["Instalación de Cámaras de Seguridad", "Ejecución, Reparación y Ampliación de Instalaciones Eléctricas Monofásicas y Trifásicas.", "Auxilios Eléctricos"],
-    images: []
+    title: "ELECTROMECÁNICA",
+    description: "Ofrecemos servicios de reparacion de automoviles y muchas cosas, como lorem ipsum e ipsum lorem",
+    icon: "fa-regular fa-wrench"
   },
   {
-    id: 5,
-    title: "ELECTRONICA DE CONTROL",
-    items: ["Instalación, Mantenimiento y Reparación de Semáforos", "Instalación, Mantenimiento y Reparación de Balizamiento LED y Sistemas Lumínicos Sonoros para Fuerzas de Seguridad, Cuerpos de Bomberos, Servicios de Salud, Grúas, Seguridad Vial", "Reparación de Electrificadores"],
-    images: []
+    title: "ELECTROMECÁNICA",
+    description: "Ofrecemos servicios de reparacion de automoviles y muchas cosas, como lorem ipsum e ipsum lorem",
+    icon: "fa-regular fa-wrench"
   },
   {
-    id: 6,
-    title: "ELECTRICIDAD INDUSTRIAL",
-    items: ["Ejecución, Reparación, Ampliación y Mantenimiento de Sistemas Eléctricos Trifásicos", "Diseño y Armado de Tableros Eléctricos Trifásicos", "Configuración de Equipos de Automatismo", "Iluminación de Plantas Industriales"],
-    images: []
-  },
-  {
-    id: 7,
-    title: "REFRIGERACIÓN COMERCIAL E INDUSTRIAL",
-    items: ["Armado, Montaje, Instalación y Puesta en Funcionamiento de Cámaras Frigoríficas", "Reparación de Cámaras Frigoríficas", "Reparación de Heladeras Comerciales", "Mantenimiento de Sistemas Frigoríficos Industriales", "Aplicación de Válvulas y Automatismos en Sistemas Frigoríficos Industriales", "Instalación, Mantenimiento y Reparación de Compresores Frigoríficos", "Instalación, Mantenimiento y Reparación de Enfriadores de Aire, Torres de Enfriamiento de Agua y Condensadores", "Montaje de Cañerías y Recipientes en Instalaciones Frigoríficas Industriales"],
-    images: []
-  },
-  {
-    id: 8,
-    title: "GENERADORES ELÉCTRICAS",
-    items: ["Asesoramiento para posible ubicación, Montaje, Instalación y Puesta en Servicio", "Tendido de Cableado de Fuerza y Señales", "Montaje, Instalación y Puesta en Servicio de Equipos", "Instalación de Accesorios para la correcta Conmutación de Cargas", "Conexionado de las Líneas del Equipo a las Líneas del Establecimiento", "Ensayos y Simulacros necesarios para garantizar el correcto funcionamiento", "Mantenimiento Preventivo y Programado de Equipos", "Reparación de Equipos"],
-    images: []
-  },
-  {
-    id: 9,
-    title: "TECNOLOGÍA DE LOS ALIMENTOS",
-    items: ["Organización y gestión de las actividades del laboratorio de producción, de los distintos procesos de producción y/o del desarrollo de nuevos productos, conformes a las normas de higiene, seguridad y ambiente en el procesamiento de los alimentos", "Realización e interpretación , análisis y ensayos organolépticos, físicos, químicos, fisicoquímicos y microbiológicos de materias primas, insumos, materiales en proceso y productos alimenticios (de origen animal, vegetal, mineral y/o artificial), efluentes y emisiones al medio ambiente", "Aplicación, control y ejecución de normas de higiene y seguridad ambientales, inocuidad, inspección e integridad a fin de alcanzar los estándares definidos en la producción y comercialización de los distintos tipos de alimentos", "Aplicación en líneas de producción: BPM, HACCP, POES, Sellado y etiquetado de productos"],
-    images: []
+    title: "ELECTROMECÁNICA",
+    description: "Ofrecemos servicios de reparacion de automoviles y muchas cosas, como lorem ipsum e ipsum lorem",
+    icon: "fa-regular fa-wrench"
   },
 ];
 
-export default trabajos;
+export default servicios;
