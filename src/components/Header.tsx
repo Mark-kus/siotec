863b9a54d9ef8f0f8bfebf7288a0a28b33eb9591
mark-kus <tignanellimarco@gmail.com>
2023-10-17 16:12:26 -0300

    fix header sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import title from '/siotec.svg?url'
 const Header: React.FC = () => {
   const [showMenu, setShowMenu] = useState<boolean>(false)
 
-  const hamburgerCls = "my-1 block rounded-sm bg-white transition-all duration-400"
+  const hamburgerCls = "block rounded-sm bg-white transition-all duration-400 h-0.5 w-5"
 
   return (
     <header className="md:flex md:justify-between fixed w-full z-10 bg-light-header-background dark:bg-dark-header-background text-light-header-text shadow">
@@ -17,12 +17,12 @@ const Header: React.FC = () => {
         </a>
 
         <button
-          className="h-5 w-5 mr-4 md:hidden"
+          className="h-5 w-5 md:hidden"
           onClick={() => setShowMenu(!showMenu)}
         >
-          <span className={`${showMenu ? "rotate-45 translate-y-1.5" : ""} ${hamburgerCls}`}></span>
-          <span className={`${showMenu ? "opacity-0" : ""} ${hamburgerCls}`}></span>
-          <span className={`${showMenu ? "-rotate-45 -translate-y-1.5" : ""} ${hamburgerCls}`}></span>
+          <span className={`${showMenu ? "rotate-45 translate-y-1.5 :" : ""} ${hamburgerCls}`}></span>
+          <span className={`${showMenu ? "opacity-0 mt-1" : "mt-1"} ${hamburgerCls}`}></span>
+          <span className={`${showMenu ? "-rotate-45 -translate-y-1.5 mt-1" : "mt-1"} ${hamburgerCls}`}></span>
         </button>
       </div>
 
